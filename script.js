ba250fff21bf93cea8604a9d76fdd50e7aca84f1
G-A223 <gunko.anastassiya@yandex.ru>
2024-10-23 16:20:42 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2000,
+    delay: 400
+});
+
+ScrollReveal().reveal('.bg-image', { delay: 400, origin: 'top'});
+ScrollReveal().reveal('.about-image', { delay: 500 });
+ScrollReveal().reveal('.text-box', { delay: 550 });
